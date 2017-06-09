80ebc3d5ebb4cef7c37bd4d66590b2f5cba35a34
Zohreh Sadeghi <zsadeghi@uw.edu>
2017-06-08 20:23:33 -0700

    Counter

---
+++ intro/src/me/theyinspire/Counter.java
@@ -0,0 +1,15 @@
+package me.theyinspire;
+
+/**
+ * Created by zohreh on 6/8/17.
+ */
+public class Counter {
+    public static void main(String[] arx) {
+        int i;
+        i = 0;
+        while (i < 100) {
+            i = i + 1;
+            System.out.println(i);
+        }
+    }
+}
